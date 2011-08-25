5e7936be59085fddfe54c5bea7121de5ca476ef9
Aliaksei Lahachou <aliaksei.lahachou@gmail.com>
2011-08-25 10:54:23 +0200

    Added EPL license.

--- m2e-jaxws-tests/src/com/brandmaker/m2e/jaxws/tests/JaxwsImportGenerationTest.java
+++ m2e-jaxws-tests/src/com/brandmaker/m2e/jaxws/tests/JaxwsImportGenerationTest.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (C) 2011 BrandMaker GmbH
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Aliaksei Lahachou (BrandMaker GmbH)
+ *******************************************************************************/
+
 package com.brandmaker.m2e.jaxws.tests;
 
 import org.eclipse.core.resources.IFile;
